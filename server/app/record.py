73b07fbf218e8bd4a19b2ba3a229c6b22de2f651
t880216t <562746248@qq.com>
2018-09-29 22:35:52 +0800

    上传图片

--- server/app/record.py
+++ server/app/record.py
@@ -45,3 +45,71 @@ def submitRecord():
     else:
         response = cors_response({'code': 10001, 'msg': '添加失败'})
         return response
+
+@app.route('/uploadRecordImage', methods=['POST'])
+def uploadRecordImage():
+    upload_file = request.files["Image"]
+    if upload_file:
+        # 连接
+        db = MySQLdb.connect(database_host, database_username, database_password, database1)
+        dbc = db.cursor()
+        # 编码问题
+        db.set_character_set('utf8')
+        dbc.execute('SET NAMES utf8;')
+        dbc.execute('SET CHARACTER SET utf8;')
+        dbc.execute('SET character_set_connection=utf8;')
+
+        filename = secure_filename(upload_file.filename)
+        # 保存文件
+        upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
+        # 入库
+        sql = 'insert into attachment (file_name,url) VALUES (%s,%s)'
+        dbc.execute(sql, (filename, '/static/uploads/' + filename))
+        db.commit()
+        # 取id
+        getId = 'SELECT LAST_INSERT_ID()'
+        dbc.execute(getId)
+        ids = dbc.fetchone()
+        attachment_id = ids[0]
+        dbc.close()
+        db.close()
+        response = cors_response({'code': 0, 'msg': '上传成功', 'content': {'attachment_id': attachment_id}})
+        return response
+    else:
+        response = cors_response({'code': 10001, 'msg': '上传失败'})
+        return response
+
+@app.route('/uploadRecordImages', methods=['POST'])
+def uploadRecordImages():
+    upload_files = request.files.getlist('file')
+    if upload_files:
+        # 连接
+        db = MySQLdb.connect(database_host, database_username, database_password, database1)
+        dbc = db.cursor()
+        # 编码问题
+        db.set_character_set('utf8')
+        dbc.execute('SET NAMES utf8;')
+        dbc.execute('SET CHARACTER SET utf8;')
+        dbc.execute('SET character_set_connection=utf8;')
+        attachment_ids = []
+        for upload_file in upload_files:
+            filename = secure_filename(upload_file.filename)
+            # 保存文件
+            upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
+            # 入库
+            sql = 'insert into attachment (file_name,url) VALUES (%s,%s)'
+            dbc.execute(sql, (filename, '/static/uploads/' + filename))
+            db.commit()
+            # 取id
+            getId = 'SELECT LAST_INSERT_ID()'
+            dbc.execute(getId)
+            ids = dbc.fetchone()
+            attachment_id = ids[0]
+            attachment_ids.append(attachment_id)
+        dbc.close()
+        db.close()
+        response = cors_response({'code': 0, 'msg': '上传成功', 'content': {'attachment_ids': attachment_ids}})
+        return response
+    else:
+        response = cors_response({'code': 10001, 'msg': '上传失败'})
+        return response
